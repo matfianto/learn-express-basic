698ced06075b0152223a9510aba53d3b4fe4abc6
Rahmat Alfianto <rrahmatalfianto@gmail.com>
2021-12-06 18:09:57 +0700

    added middelware and routing basic

--- index.js
+++ index.js
@@ -22,20 +22,72 @@ const data = [
     }
 ]
 
-app.get("/", (req, res) => res.send("<h1>hello world<h1>"));
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    const login = true;
+    if(login) {
+        next();
+    } else {
+        res.json({error: "anda belum login !"})
+    }
+}
+
+
+app.set("view engine", "ejs");
+
+app.get("/greeting", (req,res) => {
+    let name = "Rahmat Alfianto"
+    const nameFromUser = req.query.name;
+    if(nameFromUser) name = nameFromUser
+    res.render("index", {name: name})
+})
+
+// app.use(logger);
+
+// app.get("/", (req, res) => res.send("<h1>hello world<h1>"));
 app.get("/woi", (req, res) => res.send("<h1>hello woi org<h1>"));
 app.get("/woi/coi", (req, res) => res.send("<h1>hello coi<h1>"));
 
-app.get("/berita", (req, res) => res.json(data));
-app.get("/berita/:id", (req, res) => {
-    const idBerita = request.params.id;
+// app.get("/berita", (req, res) => res.json(data));
+// app.get("/berita/:id", (req, res) => {
+//     const idBerita = request.params.id;
+//     const detailBerita = data.filter((item) => item.id === parseInt(idBerita));
+//     if(detailBerita.length === 0)  {
+//         response.json({error : `jumlah berita hanya ada ${data.length}`});
+//     } else {
+//         response.json(detailBerita)
+//     }
+// });
+
+app.get("/", function(req,res,next){
+    logger(req,res,next)
+}, (req,res) => res.send("Rahmat News"));
+
+app.get("/berita", (req,res,next)=>{
+    res.json(data)
+    logger(req,res,next)
+    next()
+}, function(req,res,next){
+    console.log("Rahmattttt");
+    next()
+}, function(req,res,next){
+    console.log("Apaaaa");
+    // next() //tidak perlu
+});
+
+
+app.get("/berita/:idBerita", (req, res) => {
+    const idBerita = req.params.idBerita;
     const detailBerita = data.filter((item) => item.id === parseInt(idBerita));
     if(detailBerita.length === 0)  {
-        response.json({error : `jumlah berita hanya ada ${data.length}`});
+        res.json({error : `jumlah berita hanya ada ${data.length}`});
     } else {
-        response.json(detailBerita)
+        res.json(detailBerita)
     }
 });
+
+// app.get("/category")
+
 
 app.listen(port, () =>console.log("app berjalan pada port " + port)); //best practice
 // app.listen(port, console.log("app berjalan di port " + port));
